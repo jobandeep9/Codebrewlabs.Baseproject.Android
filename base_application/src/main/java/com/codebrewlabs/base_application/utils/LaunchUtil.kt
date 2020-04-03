088a9d92ae3083fe17827e9ca789ce4528f0650a
Gundeep Rattan <gundeep94@gmail.com>
2020-04-03 17:04:39 +0530

    launching code changed

--- base_application/src/main/java/com/codebrewlabs/base_application/utils/LaunchUtil.kt
+++ base_application/src/main/java/com/codebrewlabs/base_application/utils/LaunchUtil.kt
@@ -2,16 +2,18 @@ package com.codebrewlabs.base_application.utils
 
 import android.content.Context
 import android.content.Intent
+import com.codebrewlabs.base_application.ui.loginsignup.SignUpActivity
 
 class LaunchUtil {
     companion object {
         @JvmStatic
         fun launchBaseApp(context: Context) {
             try {
-                val intent = Intent(
-                    context,
-                    Class.forName("com.codebrewlabs.base_application.ui.loginsignup.SignUpActivity")
-                )
+//                val intent = Intent(
+//                    context,
+//                    Class.forName("com.codebrewlabs.base_application.ui.loginsignup.SignUpActivity")
+//                )
+                val intent = Intent(context,SignUpActivity::class.java)
                 context.startActivity(intent)
             } catch (e: ClassNotFoundException) {
                 print(e.message)
