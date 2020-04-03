5ac9ce1c2e3bc504119de369be77828a68731530
Gundeep Rattan <gundeep94@gmail.com>
2020-04-03 15:18:13 +0530

    added method to launch our base app

---
+++ app/src/main/java/com/baseapplication/ui/main/LaunchActivity.kt
@@ -0,0 +1,24 @@
+package com.baseapplication.ui.main
+
+import androidx.databinding.DataBindingUtil
+import com.baseapplication.R
+import com.baseapplication.databinding.ActivityLaunchBinding
+import com.codebrewlabs.base_application.ui.base.BaseActivity
+import com.codebrewlabs.base_application.utils.LaunchUtil
+
+class LaunchActivity : BaseActivity<ActivityLaunchBinding>() {
+
+    override fun initialiseItems() {
+        LaunchUtil.launchBaseApp(this)
+    }
+
+    override fun setListeners() {
+    }
+
+    override fun setData() {
+    }
+
+    override fun initViewDataBinding(): ActivityLaunchBinding {
+        return DataBindingUtil.setContentView(this, R.layout.activity_launch)
+    }
+}
---
+++ base_application/src/main/java/com/codebrewlabs/base_application/utils/LaunchUtil.kt
@@ -0,0 +1,22 @@
+package com.codebrewlabs.base_application.utils
+
+import android.content.Context
+import android.content.Intent
+
+class LaunchUtil {
+    companion object {
+        @JvmStatic
+        fun launchBaseApp(context: Context) {
+            try {
+                val intent = Intent(
+                    context,
+                    Class.forName("com.codebrewlabs.base_application.ui.loginsignup.SignUpActivity")
+                )
+                context.startActivity(intent)
+            } catch (e: ClassNotFoundException) {
+                print(e.message)
+            }
+
+        }
+    }
+}
